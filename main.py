dd0832f08ee0d7858b92e45b2bf05a79ef6e7c39
davijr28 <77064981+davijr28@users.noreply.github.com>
2022-11-24 01:21:44 +0000

    acesso ao banco de dados (ainda com erro ao inserir)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-#Importar o arquivo 'Pessoa.py' no diretório 'model'
+#Importar arquivos de outros diretórios
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 #Exemplo de uso
 poyatos = Pessoa (1, "Henrique Poyatos")
@@ -7,3 +9,23 @@ print(poyatos)
 
 #Mostrar apenas o nome
 print(poyatos.nome)
+print("ACESSO AO BANCO: ")
+
+#Chama o objeto de banco de dados
+db = Database()
+
+#Instancia o objeto
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Carregar um lista com o banco de dados
+pessoas = pessoaDAO.getAll(orderBy = True)
+for pessoa in pessoas:
+  print(pessoa)
+
+#Criar um objeto com qualquer ator/atriz/diretor/diretora
+novo = Pessoa(0, "Denzel Washington")
+pessoaDAO.save(novo)
+
+pessoas = pessoaDAO.getAll(orderBy = True)
+for pessoa in pessoas:
+  print(pessoa)
